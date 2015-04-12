ee67074c9e19900b42f1e97704ff83a68e6e16c8
vincent <vincent.lefoulon@free.fr>
2015-04-12 16:26:39 +0200

    Clean analysis

--- mpf/config.py
+++ mpf/config.py
@@ -1,5 +1,6 @@
 from os.path import join, normpath, realpath
 
+
 # Paths
 ROOT = normpath(join(realpath(__file__), "../.."))
 
@@ -11,4 +12,22 @@ DATABASE_PATH = join(DATABASE_DIR, "mpf.db")
 CACHE_DIR = join(DATA_DIR, "cache")
 
 VIEWS_DIR = join(ROOT, "views")
-PROD_DIR = join(VIEWS_DIR, "production")
+PROD_VIEWS_DIR = join(VIEWS_DIR, "production")
+CRUDE_PROD_VIEWS_DIR = join(PROD_VIEWS_DIR, "crude")
+SMOOTHED_PROD_VIEWS_DIR = join(PROD_VIEWS_DIR, "smoothed")
+LINREG_PROD_VIEWS_DIR = join(PROD_VIEWS_DIR, "linreg")
+
+
+# Labels/keys
+CONS_LBL = "cons"
+COW_LBL = "cow"
+CRUDE_LBL = "crude"
+DATES_LBL = "dates"
+DAYS_LBL = "days"
+LACT_LBL = "lact"
+PRODS_LBL = "prods"
+
+CONS_KEY = (CRUDE_LBL, CONS_LBL)
+DATES_KEY = (CRUDE_LBL, DATES_LBL)
+DAYS_KEY = (CRUDE_LBL, DAYS_LBL)
+PRODS_KEY = (CRUDE_LBL, PRODS_LBL)
